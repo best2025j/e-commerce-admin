b9afcdb8b0acd37014610b10d2bc2602cea7cbb3
best2025j <audugodtime95@gmai.com>
2023-08-09 14:48:12 +0100

    feat:changes

--- component/home.jsx
+++ component/home.jsx
@@ -1,13 +1,16 @@
+"use client";
 import React from "react";
 
 export default function Home() {
-  return (
-    <div className="bg-blue-900 h-screen w-screen flex items-center">
-      <div className="w-full text-center">
-        <button className="px-4 p-2 rounded-full bg-white">
-          signIn with google
-        </button>
+  {
+    return (
+      <div className="bg-blue-900 h-screen w-screen flex items-center">
+        <div className="w-full text-center">
+          <button className="px-4 p-2 rounded-full bg-white text-sm font-medium">
+            signIn with google
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,10 @@
 import React from "react";
-import { useSession, signIn, signOut } from "next-auth/react";
+import Home from "../../component/home";
 
-export default function page() {
-  const { data: session } = useSession;
-  if (!session) {
-    return (
-      <div className="bg-blue-900 h-screen w-screen flex items-center">
-        <div className="w-full text-center">
-          <button
-            onClick={() => signIn("google")}
-            className="px-4 p-2 rounded-full bg-white text-sm font-medium"
-          >
-            signIn with google
-          </button>
-        </div>
-      </div>
-    );
-  }
-
-  return <> Signed in as {session.user.email}</>;
+export default function Page() {
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
